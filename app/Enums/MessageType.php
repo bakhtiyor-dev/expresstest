45f01ed63a0da4df6de26a0a4a6027e95ac2801e
Bakhtiyor <alijanovbakhtiyor@gmail.com>
2023-07-26 13:53:10 +0500

    some fixes

--- app/Enums/MessageType.php
+++ app/Enums/MessageType.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Enums;
+
 enum MessageType: string
 {
     case TELEGRAM = 'telegram';
--- app/MessageHandler.php
+++ app/MessageHandler.php
@@ -11,20 +11,16 @@ class MessageHandler
     /**w
      * Handle a payload, send the message and return result
      */
-    public function handle(MessageInterface $payload, MessengerFactory $factory, Logger $logger): array
+    public function handle(MessageInterface $payload, MessengerFactory $messengerFactory, Logger $logger): array
     {
         try {
             $type = $payload->getType($payload->getMessage());
-            return [
-                'success' => true,
-                'payload' => $factory->getMessenger($type)->send($payload->getMessage())
-            ];
+            $response = $messengerFactory->getMessenger($type)->send($payload->getMessage());
+
+            return ['success' => true, 'payload' => $response];
         } catch (Exception $exception) {
             $logger->error($exception->getMessage());
-            return [
-                'success' => false,
-                'payload' => $exception->getMessage()
-            ];
+            return ['success' => false, 'payload' => $exception->getMessage()];
         }
     }
 }
